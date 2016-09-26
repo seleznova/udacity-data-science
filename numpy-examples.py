f3266a7baf2426451a2c348b0117ef29ca95d1be
Mary Kuzmenko <mary.kuzmenko@gmail.com>
2016-09-26 22:25:34 +0300

    Create numpy-examples.py

---
+++ numpy-examples.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+if True:
+    array = np.array([1, 4, 5, 8], float)
+    print array
+    print ""
+    array = np.array([[1, 2, 3], [4, 5, 6]], float)
+    print array
+
+if True:
+    array = np.array([1, 4, 5, 8], float)
+    print array
+    print ""
+    print array[1]
+    print ""
+    print array[:2]
+    print ""
+    array[1] = 5.0
+    print array[1]
+
